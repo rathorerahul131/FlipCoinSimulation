e57b8aec863e019edbef4b18f2e9adf8ca8f46ea
neelrahulr <rathorerahul131@gmail.com>
2020-11-23 16:06:34 +0530

    Refactored: Flipped the coin to get Winner

--- coinSimulationProblem.sh
+++ coinSimulationProblem.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Welcome to the Flip Coin Simulation Game"
+
+toss=$((RANDOM%2)) # toss will randomly store either 0 or 1
+
+if [[ $toss -eq 0 ]]
+then
+	echo "Tail Wins"
+else
+	echo "Head Wins"
+fi
