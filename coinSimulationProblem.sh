14c9b09299fa623bf7403c936da62db51faf08e9
neelrahulr <rathorerahul131@gmail.com>
2020-11-23 16:17:16 +0530

    Refactored: Flipped coin in loop

--- coinSimulationProblem.sh
+++ coinSimulationProblem.sh
@@ -2,11 +2,20 @@
 
 echo "Welcome to the Flip Coin Simulation Game"
 
-toss=$((RANDOM%2)) # toss will randomly store either 0 or 1
+noOfHead=0 #variable to store the count of Head wins
+noOfTail=0 #variable to store the count of Tail wins
 
-if [[ $toss -eq 0 ]]
-then
-	echo "Tail Wins"
-else
-	echo "Head Wins"
-fi
+for ((i=0;i<20;i++))
+do
+	toss=$((RANDOM%2)) # toss will randomly store either 0 or 1
+	if [[ $toss -eq 0 ]]
+	then
+		((noOfTail++)) 
+	else
+		((noOfHead++))
+	fi
+	
+done
+
+echo "Head has won $noOfHead times"
+echo "Tail has won $noOfTail times"
